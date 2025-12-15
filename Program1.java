0a23218726bc14dd7a6b58dc585fc70232b34522
Batch1 <Batch1@VITUNIVERSITY.LOCAL>
2025-12-15 12:21:28 +0530

    added new file

---
+++ Program1.java
@@ -0,0 +1,7 @@
+public class Program1{
+   public static void main(String[] args) {
+       int num1 = 5, num2 = 10, sum;
+       sum = num1 + num2;
+       System.out.println("Sum of these numbers: " + sum);
+   }
+}
